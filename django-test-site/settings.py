b363f095c5e7b6fce5d157187f06981d3e1a2b08
ilia.comarov <ilia.comarov@ffwagency.com>
2017-10-06 16:27:53 +0300

    feature-8

--- django-test-site/settings.py
+++ django-test-site/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
-MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = ( os.path.join('media'), )
+MEDIA_URL = '/'
+MEDIAFILES_DIRS = ( os.path.join(''), )
