a76f6e111b56c35e4ad77a33c76a07acd68ffaff
ilia.comarov <ilia.comarov@ffwagency.com>
2017-10-06 13:09:42 +0300

    feature-8

--- django-test-site/settings.py
+++ django-test-site/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
-
+STATICFILES_DIRS = ( os.path.join('static'), )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
+MEDIAFILES_DIRS = ( os.path.join('media'), )
